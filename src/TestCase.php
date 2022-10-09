16e1bb530981ecdb07c1fb7d3f4be6ff694d2070
Charles Sena <charles.sena.sa@gmail.com>
2022-10-08 22:18:23 -0300

    2022-10-10 11:38:17

--- src/TestCase.php
+++ src/TestCase.php
@@ -1,24 +1,5 @@
 <?php
 
-class FailMessage {
-  function __construct(
-    string $className,
-    string $testName,
-    Error|Exception|AssertionError $exception
-  )
-  {
-    $this->className = $className;
-    $this->testName = $testName;
-    $this->exception = $exception;
-  }
-
-  function __toString()
-  {
-    return $this->className.'.'.  $this->testName.'.'.  $this->exception::class.': "'. $this->exception->getMessage().'"';
-  }
-
-}
-
 class TestCase {
   function __construct(string $name)
   {
